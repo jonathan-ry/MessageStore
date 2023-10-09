f100136de236e7bf1d69440267eaee7c1ba9819d
jonathan-ry <rubiano.jonathanr@gmail.com>
2023-10-09 19:37:40 +0800

    MessagingAPI First Commit

---
+++ MessagingStore/Controllers/MessageController.cs
@@ -0,0 +1,33 @@
+﻿using MessagingStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MessagingStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : Controller
+    {
+        [HttpPost("Send")]
+        public IActionResult Send(MessageDetail message)
+        {
+            if (message != null) return Ok(message);
+
+            return BadRequest();
+        }
+
+        [HttpGet("Get/{id}")]
+        public IActionResult Get(int id) 
+        {
+            MessageDetail message = new MessageDetail
+            {
+                Id = id,
+                Sender = "Accenture Team",
+                Recipient = "Juan Dela Cruz",
+                Message = "Welcome to Accenture, Juan Dela Cruz"
+            };
+            
+            return Ok(message);
+
+        }
+    }
+}
---
+++ MessagingStore/Models/MessageDetail.cs
@@ -0,0 +1,10 @@
+﻿namespace MessagingStore.Models
+{
+    public class MessageDetail
+    {
+        public int Id { get; set; }
+        public string? Sender { get; set; }
+        public string? Recipient { get; set; }
+        public string? Message { get; set; }
+    }
+}
